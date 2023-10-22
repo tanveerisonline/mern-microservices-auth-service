9772b4f6c2b2cf8a32c4b6fd444958a3aff3ac51
Tanveer Ahmad <tanveerisonline@gmail.com>
2023-10-22 22:16:13 +0530

    Add configuration using .env

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+import { config } from 'dotenv';
+config();
+
+const { PORT } = process.env;
+export const Config = {
+    PORT,
+};
--- src/server.ts
+++
@@ -1,5 +0,0 @@
-const obj = {
-    property1: 'Hello, World!',
-};
-const pro1 = obj.property1;
-pro1;
